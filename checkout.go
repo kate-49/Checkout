cdc4dfd066222b88f44df1626934a34684f2513d
kate smith <kateesmith4994@gmail.com>
2020-11-13 16:54:51 +0000

    passes acceptance test 3

--- checkout.go
+++ checkout.go
@@ -5,9 +5,19 @@ import "unicode"
 func Run(input string) int {
 	var answer int
 
+	var prices = map[string]int{"A": 50, "B": 30, "C": 20, "D": 15}
+
 	for _, letter := range input {
 		if unicode.IsLower(letter) {
 			answer = -1
+		} else {
+			for priceLetter, price := range prices {
+				stringletter := string(letter)
+				if priceLetter == stringletter {
+					answer += price
+
+				}
+			}
 		}
 	}
 	return answer
