0603e1cdab9f35a9c7177762583fecda68e4248e
kate smith <kateesmith4994@gmail.com>
2020-11-13 16:24:50 +0000

    failing first acceptance test

---
+++ checkout_test.go
@@ -0,0 +1,18 @@
+package checkout
+
+import "testing"
+
+func TestRun(t *testing.T) {
+
+	t.Run("Test for first acceptance criteria", func(t *testing.T) {
+		got := Run("aBc")
+		want := -1
+		assertEquals(t, got, want)
+	})
+}
+
+func assertEquals(t *testing.T, got int, want int) {
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
